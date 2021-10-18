70a6008c3bcb939ead53a41ec6f59ad1e7d4e1ae
MikiKru <michal_kruczkowski@o2.pl>
2021-10-18 11:02:41 +0200

    MASTER implements User fields and methods

--- src/pl/pbs/User.java
+++ src/pl/pbs/User.java
@@ -1,5 +1,38 @@
 package pl.pbs;
 
+
+import java.time.LocalDateTime;
+
 public class User {
     public String name;
+    public LocalDateTime registrationDateTime;
+
+    public User(String name, LocalDateTime registrationDateTime) {
+        this.name = name;
+        this.registrationDateTime = registrationDateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", registrationDateTime=" + registrationDateTime +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getRegistrationDateTime() {
+        return registrationDateTime;
+    }
+
+    public void setRegistrationDateTime(LocalDateTime registrationDateTime) {
+        this.registrationDateTime = registrationDateTime;
+    }
 }
