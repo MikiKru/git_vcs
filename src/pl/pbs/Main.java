d71ae8f8265fbe52ee9489a8bc16579c514e5577
JonLegorburu <jon.legorburu@alumni.mondragon.edu>
2021-10-18 12:26:54 +0200

    Jon's commit

--- src/pl/pbs/Main.java
+++ src/pl/pbs/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("END");
         System.out.println("I'm Samuel");
+        System.out.println("I'm JonLegorburu");
     }
 }
